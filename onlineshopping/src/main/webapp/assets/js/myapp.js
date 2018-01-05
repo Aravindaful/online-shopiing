29ef6ebbcec960d6fc9d0a79c40589a346975322
Supun Aravinda <aravinda.airhorse@gmail.com>
2018-01-05 12:56:31 +0530

    commit successfully

---
+++ onlineshopping/src/main/webapp/assets/js/myapp.js
@@ -0,0 +1,11 @@
+$(function(){
+	switch(menu){
+	case 'About US':
+		$("#about").addClass("active"); break;
+	case 'Contact':
+		$("#contact").addClass("active"); break;
+		
+		default :
+			$("#home").addClass("active");break;
+	}
+});
